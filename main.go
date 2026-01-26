6d7e537abae7ea07f9ac5f059a008b0413af1ddb
preetbhingradiya <preetbhingradiya6@gmail.com>
2026-01-26 14:04:34 +0530

    defer and panic

--- main.go
+++ main.go
@@ -8,20 +8,25 @@ import (
 func main() {
 	fmt.Println("Hello, Golang!")
 	/*
-	// 1. PACKAGE EXAMPLE
-	basics.PackageExample()
-	// 2 Over and Under Flow
-	basics.OverflowUnderflow()
-	// 3 Condition and Loop
-	basics.Loop()
-	// 4 Guessing Number Game
-	basics.GuessingNumber()
-	//5 Arrays
-	 basics.Array()
-	//6 Slices
-	 basics.Slice()
+		// 1. PACKAGE EXAMPLE
+		basics.PackageExample()
+		// 2 Over and Under Flow
+		basics.OverflowUnderflow()
+		// 3 Condition and Loop
+		basics.Loop()
+		// 4 Guessing Number Game
+		basics.GuessingNumber()
+		//5 Arrays
+		 basics.Array()
+		//6 Slices
+		 basics.Slice()
+		//7 Function
+		 basics.Method()
+		//8 Defer
+		 basics.Defer()
 	*/
 
-	//7 Function
-	basics.Method()
+	//9 Panic and Recover
+	basics.Panic()
+
 }
